da6a95aeeb4851005f8de251a49540c1ae6b5bb2
Arian <88984301+Arian-Ott@users.noreply.github.com>
2024-08-18 22:09:16 +0200

    Started Fast API ADDED database.py, can access CouchDB, delete, add, and maintain documents
    ADDED Docker support. API is tested.
    
    ADDED couchbase compression. Every entry will be compressed with zlib

--- app/chat.py
+++ app/chat.py
@@ -2,7 +2,6 @@ from fastapi import APIRouter
 
 
 class Chat:
-
     def __init__(self):
         self.router = APIRouter(prefix="/chat", tags=["Chat"])
         self.router.add_api_route("/test/", self.hello, methods=["GET"])
--- app/main.py
+++ app/main.py
@@ -1,24 +1,14 @@
 import uvicorn
 from fastapi import FastAPI
 
-from chat import Chat
-from database import FileUpload
+from .chat import Chat
+from .database import DocumentDB
 
 app = FastAPI()
 cht = Chat()
-db = FileUpload()
+db = DocumentDB()
 app.include_router(cht.router)
 app.include_router(db.router)
+
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
-    # r = retriever.Extractor("../data")
-    # r.extract()
-    # r.to_txt()
-    # vectorstore = vector.Vectorstore("192.168.1.77", "text-embedding-3-small")
-
-    # col = collection.Collection(vectorstore, "text-embedding-3-small")
-    # emb = embedding.Embedding(vectorstore)
-    #emb.add_text("Ich esse einen Apfel")
-
-    # result = emb.embedding()
-    #col.upload(result)
+    uvicorn.run(app, host="0.0.0.0", port=6969)
